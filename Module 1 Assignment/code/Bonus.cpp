ae13e54515227793bd4b26c14a02b528fd797f2c
Maya <143032303+Maya-Anber@users.noreply.github.com>
2024-09-11 02:35:07 +0300

    Bonus.cpp

--- Module 1 Assignment/code/Bonus.cpp
+++ Module 1 Assignment/code/Bonus.cpp
@@ -11,23 +11,23 @@ int main() {
     float diameter1, price1, diameter2, price2;
     float pricePerSquareInch1, pricePerSquareInch2;
 
-    std::cout << "Enter the diameter (in inches) and price for the first pizza: ";
-    std::cin >> diameter1 >> price1;
-    std::cout << "Enter the diameter (in inches) and price for the second pizza: ";
-    std::cin >> diameter2 >> price2;
+   cout << "Enter the diameter (in inches) and price for the first pizza: ";
+   cin >> diameter1 >> price1;
+   cout << "Enter the diameter (in inches) and price for the second pizza: ";
+   cin >> diameter2 >> price2;
 
     pricePerSquareInch1 = calculatePricePerSquareInch(diameter1, price1);
     pricePerSquareInch2 = calculatePricePerSquareInch(diameter2, price2);
 
-    std::cout << "Price per square inch for the first pizza: $" << pricePerSquareInch1 << std::endl;
-    std::cout << "Price per square inch for the second pizza: $" << pricePerSquareInch2 << std::endl;
+   cout << "Price per square inch for the first pizza: $" << pricePerSquareInch1 <<endl;
+   cout << "Price per square inch for the second pizza: $" << pricePerSquareInch2 <<endl;
 
     if (pricePerSquareInch1 < pricePerSquareInch2) {
-        std::cout << "The first pizza is the better buy." << std::endl;
+       cout << "The first pizza is the better buy." <<endl;
     } else if (pricePerSquareInch2 < pricePerSquareInch1) {
-        std::cout << "The second pizza is the better buy." << std::endl;
+       cout << "The second pizza is the better buy." <<endl;
     } else {
-        std::cout << "Both pizzas have the same price per square inch. The smaller pizza is considered the better buy." << std::endl;
+       cout << "Both pizzas have the same price per square inch. The smaller pizza is considered the better buy." <<endl;
     }
 
     return 0;
