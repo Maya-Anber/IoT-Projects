28d0596059f4a02da896ecf7a44671172838c717
Maya <143032303+Maya-Anber@users.noreply.github.com>
2024-09-11 03:01:04 +0300

    Update Problem 7.c

--- Module 1 Assignment/code/Problem 7.c
+++ Module 1 Assignment/code/Problem 7.c
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 const double G = 6.673e-8;  // Universal gravitational constant (cm^3)/(g*sec^2)
 
 double calculateGravitationalForce(double mass1, double mass2, double distance) {
     if (distance == 0) {
-        std::cerr << "Distance cannot be zero." << std::endl;
+        cout << "Distance cannot be zero." << endl;
         return -1;
     }
     return (G * mass1 * mass2) / (distance * distance);
@@ -16,23 +18,23 @@ int main() {
     char repeat;
 
     do {
-        std::cout << "Enter the mass of the first body (in grams): ";
-        std::cin >> mass1;
+        cout << "Enter the mass of the first body (in grams): ";
+        cin >> mass1;
 
-        std::cout << "Enter the mass of the second body (in grams): ";
-        std::cin >> mass2;
+        cout << "Enter the mass of the second body (in grams): ";
+        cin >> mass2;
 
-        std::cout << "Enter the distance between the bodies (in centimeters): ";
-        std::cin >> distance;
+        cout << "Enter the distance between the bodies (in centimeters): ";
+        cin >> distance;
 
         force = calculateGravitationalForce(mass1, mass2, distance);
 
         if (force != -1) {
-            std::cout << "The gravitational force between the two bodies is: " << force << " dynes" << std::endl;
+            cout << "The gravitational force between the two bodies is: " << force << " dynes" << endl;
         }
 
-        std::cout << "Do you want to perform another calculation? (y/n): ";
-        std::cin >> repeat;
+        cout << "Do you want to perform another calculation? (y/n): ";
+        cin >> repeat;
 
     } while (repeat == 'y' || repeat == 'Y');
 
